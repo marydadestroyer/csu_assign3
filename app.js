d4cd0cdf80a0ae8a084b32af5959e625e81a7a6c
Mary Harrell <harrell_mary@columbusstate.edu>
2019-12-03 18:30:05 +0000

    trying something new12

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-//var http = require('http');
-//var path = require('path');
+var http = require('http');
+var https = require('https');
+var request = require('request');
+var path = require('path');
+
 var express = require("express");
 var ejs = require('ejs');
 var fetch = require('node-fetch'); 
-//var bodyParser = require("body-parser");
+
+var bodyParser = require("body-parser");
 //const app = express();
 var app = express();
 //const port = process.env.PORT || 3000;
 var port = process.env.PORT || 3000;
 
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 //app.use(express.static("public"));
 //app.use(bodyParser.urlencoded({ encoded: true}));
@@ -18,25 +22,10 @@ app.use(express.static(_dirmane +'/public'));
 var img = '';
 var title = '';
 
-app.get('/', function(req, res){
-    fetchComic();
-    res.render('index',{img:img, title:title});
-});
 
 
 
-function fetchComic(){
-    fetch ('https://xkcd.com/614/info.0.json'
-        .then(res => res.json())
-        .then(thing =>{
-            img = thing.img;
-            title= thing.title;
 
-        }).catch(err =>{
-            res.redirect('/error');
-        })
-    )
-}
 
 http.createServer(app).listen(port, function(){
 
