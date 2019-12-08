1f2078e9978e32ffdfb12793cf7c42ab6b902c29
Mary Harrell <harrell_mary@columbusstate.edu>
2019-12-08 14:59:30 +0000

    changessssy

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 var http = require('http');
-var request = require('request');
-//var path = require('path');
-var express = require("express");
-var ejs = require('ejs');
-var fetch = require('node-fetch'); 
+var path = require('path');
+const express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
-var port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ encoded: true}));
-app.use(express.static(_dirmane +'/public'));
+var task = ["clean", "cook"];
+var complete = ["eat", "sleep"];
 
 var img = '';
 var title = '';
